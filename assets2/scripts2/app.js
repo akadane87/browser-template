7aba4196957729bc1da50cff98e3184cfebc971e
akadane87 <akadane87@gmail.com>
2017-02-02 04:32:02 -0500

    points to alex's heroku

--- assets2/scripts2/app.js
+++ assets2/scripts2/app.js
@@ -5,6 +5,7 @@ const app = {
 };
 
 module.exports = {
-  host: 'https://aqueous-atoll-85096.herokuapp.com/',
+  // host: 'https://aqueous-atoll-85096.herokuapp.com/',
+  host: 'https://radiant-caverns-96221.herokuapp.com',
   app,
 };
